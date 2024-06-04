80104b2565bf5b8de3414b0fbe588ec165c9cb63
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-06-04 17:21:24 +0200

    feat: add url path handling

--- app/server.go
+++ app/server.go
@@ -2,26 +2,52 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
+	// Set up listener on port 4221 using tcp
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
+	// Defer connection closing before exiting
+	defer l.Close()
+
+	// Accept incoming connection requests
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	// Set up buffer size and read incoming data
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading connection", err.Error())
+		os.Exit(1)
+	}
+
+	// Make string from buf byte array
+	req := string(buf)
+
+	// Split the request in parts using the defined separator
+	lines := strings.Split(req, "\r\n\r\n")
+
+	// Split the first part to get the path
+	path := strings.Split(lines[0], " ")[1]
+	// fmt.Println(path)
+
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
